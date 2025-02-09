33e7e96d85fcb95e21e0d63f2e946505c3d1ada8
Rohit Mundra <Rohit.Mundra@target.com>
2025-02-09 16:02:41 +0530

    config changes to run experiments

--- src/config.py
+++ src/config.py
@@ -42,7 +42,7 @@ class Config:
         "short_player",
         "spectators",
     ]
-    MODEL = "CNNDeep" #  "CNNBase", "CNNDeep", "CNNDeepWithSE", "CNNDeepWithCSE"
-    DATA_REDUCTION_FACTOR = 1.0
+    MODEL = "CNNDeepWithSE" #  "CNNBase", "CNNDeep", "CNNDeepWithSE", "CNNDeepWithCSE"
+    DATA_REDUCTION_FACTOR = 0.4
     LOSS_FUNCTION = "CrossEntropy" # "CrossEntropy", "NLLLoss", "MSELoss", "FocalLoss"
     SSIM_THRESHOLD = 30
